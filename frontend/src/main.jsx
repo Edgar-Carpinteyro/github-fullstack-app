ed7e34ef9ac6631b710ca90d35fd2405fbba6d3f
Edgar-Carpinteyro <carpinteyroe07@gmail.com>
2024-03-20 20:48:03 -0700

    updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,19 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
 import { AuthContextProvider } from './context/AuthContext.jsx'
-import App from './App'
+import App from "./App.jsx"
 import './index.css'
 
-function Main() {
-  return (
-    <React.StrictMode>
-      <BrowserRouter>
-        <AuthContextProvider>
-          <App />
-        </AuthContextProvider>
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-}
-
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
