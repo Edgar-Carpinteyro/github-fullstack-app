8e3a7f36d6353556222b5fd4fc64e3328ed1357d
Edgar-Carpinteyro <carpinteyroe07@gmail.com>
2024-03-20 20:59:21 -0700

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,23 @@ import session from "express-session";
 import path from "path";
 
 import "./passport/github.auth.js";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 // import authRoutes from "./db/connectMongoDB.js"
-import userRoutes from "./routes/user.route.js"
+import userRoutes from "./routes/user.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT ||  5050;
-const __dirname =  path.resolve();
+const PORT = process.env.PORT || 5050;
+const __dirname = path.resolve();
 
 app.use(
-    session(
-    {
-        secret: "s0m3s0rTOf5EcR3T", 
-        resave: false, 
-        saveUninitialized: false
-    })
+  session({
+    secret: "s0m3s0rTOf5EcR3T",
+    resave: false,
+    saveUninitialized: false,
+  })
 );
 
 app.use(passport.initialize());
@@ -31,13 +30,13 @@ app.use(passport.session());
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
-	console.log(`Server started on http://localhost:${PORT}`);
-    connectMongoDB();
+  console.log(`Server started on http://localhost:${PORT}`);
+  connectMongoDB();
 });
